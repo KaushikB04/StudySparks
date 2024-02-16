b90c90ed8085e4cc0f492d121dba9a2b2d5af7a1
Pushpak <pushpak3504@gmail.com>
2024-02-16 08:59:10 +0530

    settings fragment done

--- app/src/main/java/com/example/studysparks/SettingsFragment.java
+++ app/src/main/java/com/example/studysparks/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.studysparks;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class SettingsFragment extends Fragment {
 
+    Button loutbtn;
+    private FirebaseAuth mAuth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        loutbtn = view.findViewById(R.id.logoutbtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        loutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Intent intent = new Intent(getContext(),MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(getContext(), "Logout Successfull!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
